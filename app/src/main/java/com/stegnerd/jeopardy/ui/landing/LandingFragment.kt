313c4f47a6dfdf4982b7e7b4913416a95b5831e1
Dalton Stegner <dmstegner@gmail.com>
2020-07-08 21:36:16 -0400

    1. Add landing fragment

---
+++ app/src/main/java/com/stegnerd/jeopardy/ui/landing/LandingFragment.kt
@@ -0,0 +1,32 @@
+package com.stegnerd.jeopardy.ui.landing
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.stegnerd.jeopardy.R
+
+class LandingFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LandingFragment()
+    }
+
+    private lateinit var viewModel: LandingViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.landing_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(LandingViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/stegnerd/jeopardy/ui/landing/LandingViewModel.kt
@@ -0,0 +1,7 @@
+package com.stegnerd.jeopardy.ui.landing
+
+import androidx.lifecycle.ViewModel
+
+class LandingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
