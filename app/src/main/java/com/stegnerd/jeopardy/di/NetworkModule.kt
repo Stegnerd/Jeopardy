676f3169cfd24340d0a1cdc9110ba997525fe1f3
Dalton Stegner <dmstegner@gmail.com>
2020-07-12 11:30:21 -0400

    1. Added dagger network modeul

--- app/src/main/java/com/stegnerd/jeopardy/di/NetworkModule.kt
+++ app/src/main/java/com/stegnerd/jeopardy/di/NetworkModule.kt
@@ -1,5 +1,46 @@
 package com.stegnerd.jeopardy.di
 
+import com.stegnerd.jeopardy.data.api.ApiClient
+import com.stegnerd.jeopardy.data.api.ApiClientImpl
+import com.stegnerd.jeopardy.data.api.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
 object NetworkModule {
 
+    private const val BASE_API_URL = "\"http://jservice.io/api/\""
+
+    @Provides
+    fun provideBaseApiUrl() = BASE_API_URL
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder().client(okHttpClient)
+            .baseUrl(BASE_API_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit) = retrofit.create(ApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideApiClient(apiClient: ApiClientImpl): ApiClient = apiClient
 }
