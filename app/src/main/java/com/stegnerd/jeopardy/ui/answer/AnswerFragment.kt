172eab5a058ee6c7d529342e00d6ea2275e6b3d5
Dalton Stegner <dmstegner@gmail.com>
2020-07-26 00:28:57 -0400

    1. Added viewmodel and basic fragment setup

--- app/src/main/java/com/stegnerd/jeopardy/ui/answer/AnswerFragment.kt
+++ app/src/main/java/com/stegnerd/jeopardy/ui/answer/AnswerFragment.kt
@@ -6,27 +6,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.stegnerd.jeopardy.R
+import com.stegnerd.jeopardy.databinding.AnswerFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AnswerFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = AnswerFragment()
+    private lateinit var binding: AnswerFragmentBinding
+
+    private val answerViewModel: AnswerViewModel by viewModels()
+
+    private val args: AnswerFragmentArgs by navArgs()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        binding = AnswerFragmentBinding.inflate(inflater, container, false).apply {
+            viewModel = answerViewModel
+        }
+
+        val answerStatus = getAnswerStatus()
+        answerViewModel.isCorrect = answerStatus
+
+        return binding.root
     }
 
-    private lateinit var viewModel: AnswerViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.answer_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(AnswerViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun getAnswerStatus(): Boolean {
+        return args.correctAnswer
     }
 
 }
--- app/src/main/java/com/stegnerd/jeopardy/ui/answer/AnswerViewModel.kt
+++ app/src/main/java/com/stegnerd/jeopardy/ui/answer/AnswerViewModel.kt
@@ -1,7 +1,10 @@
 package com.stegnerd.jeopardy.ui.answer
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 
-class AnswerViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class AnswerViewModel @ViewModelInject constructor() : ViewModel() {
+    var isCorrect: Boolean = false
+
+
 }
