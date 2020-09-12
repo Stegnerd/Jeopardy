4aba95d013393615a61a3c010e781c86d73058b7
Dalton Stegner <dmstegner@gmail.com>
2020-09-12 00:22:58 -0400

    1. Added debugging into main app

--- app/src/main/java/com/stegnerd/jeopardy/JeopardyApplication.kt
+++ app/src/main/java/com/stegnerd/jeopardy/JeopardyApplication.kt
@@ -1,8 +1,19 @@
 package com.stegnerd.jeopardy
 
 import android.app.Application
+import com.stegnerd.jeopardy.util.TimberCrashTree
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class JeopardyApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+
+        if(BuildConfig.DEBUG){
+         Timber.plant(Timber.DebugTree())
+        }else {
+            Timber.plant(TimberCrashTree())
+        }
+    }
 }
