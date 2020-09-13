0704a9a0117325d4d88894e396a43c9f0e62dc08
Dalton Stegner <dmstegner@gmail.com>
2020-09-12 21:46:56 -0400

    1. Used crashlytics tree in release build

--- app/src/main/java/com/stegnerd/jeopardy/util/TimberCrashTree.kt
+++ app/src/main/java/com/stegnerd/jeopardy/util/TimberCrashTree.kt
@@ -1,6 +1,7 @@
 package com.stegnerd.jeopardy.util
 
 import android.util.Log
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import timber.log.Timber
 
 /**
@@ -8,17 +9,18 @@ import timber.log.Timber
  */
 class TimberCrashTree : Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        if(priority == Log.VERBOSE || priority == Log.DEBUG){
+        if (priority == Log.VERBOSE  || priority == Log.DEBUG) {
             return;
         }
 
-        // TODO implement firebase crashlytics
-        if(t != null){
-            if(priority == Log.ERROR){
-                Timber.e(t)
-            }else if (priority == Log.WARN){
-                Timber.w(t)
+        if (priority == Log.ERROR) {
+            FirebaseCrashlytics.getInstance().log(message)
+            // if there was a throwable record it for logs also
+            if (t != null) {
+                FirebaseCrashlytics.getInstance().recordException(t)
             }
+        } else if (priority == Log.WARN) {
+            Timber.w(t)
         }
     }
 
